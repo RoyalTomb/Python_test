030dabe8d1f4b4ff8f1f4c07a4020d8fa0925790
RoyalTomb <68623530+RoyalTomb@users.noreply.github.com>
2021-08-02 21:42:27 -0400

    if_statement ex.

---
+++ more_code_if_statement.py
@@ -0,0 +1,9 @@
+banana = True
+orange = False
+
+if banana and orange:
+    print("you have a banana and an orange")
+elif banana:
+    print("you only have a banana")
+else:
+    print("you ain't got nothing")
